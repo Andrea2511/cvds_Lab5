8941b76f06f9d487416e273245cd2e1df7396c6c
Andrea Torres <andrea.torres-trmail.escuelaing.edu.co>
2023-09-14 18:56:13 -0500

    Trabajo en clase

---
+++ src/main/java/com/cvds/servingWebContent/UserController.java
@@ -0,0 +1,53 @@
+package com.cvds.servingWebContent;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/hello")
+    //@ResponseBody
+    private String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/user/{id}")
+    @ResponseBody
+    private String getUser(@PathVariable Integer id) {
+        String uri = "https://jsonplaceholder.typicode.com/users/"+ id;
+        RestTemplate restTemplate = new RestTemplate();
+
+        User user = restTemplate.getForObject(uri, User.class);
+        System.out.println("User: " + user);
+
+        System.out.println("Userid: " + user.getId());
+        System.out.println("Name: " + user.getName());
+        System.out.println("Username: " + user.getUsername());
+        System.out.println("Email: " + user.getEmail());
+
+        Address address = user.getAddress();
+        System.out.println("Address: "
+                + address.getStreet() + ", "
+                + address.getCity() + ", "
+                + address.getZipcode()
+        );
+
+        Geo geo = address.getGeo();
+        System.out.println("Geo Lat: "
+                + geo.getLat() + ", Geo Lng: "
+                + geo.getLng()
+        );
+
+        Company company = user.getCompany();
+        System.out.println("Company: "
+                + company.getName() + ", "
+                + company.getCatchPhrase() + ", "
+                + company.getBs()
+        );
+
+        return "User detail page.";
+    }
+}
